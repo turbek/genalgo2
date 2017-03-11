cff51538e6f6149fb53deebac5f803863a5f8500
turbek <turbek93@gmail.com>
2017-03-11 18:43:56 +0100

    gen count and gen+ on click added

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 var POPMAX = 600;
 var POPCOUNT = 1;
 var GENEMAX = 10000;
-var MUTATION = 0.02;
+var MUTATION = 0.03;
 var TIME = 0;
-var TIMEMAX = 2000;
+var TIMEMAX = 4000;
 var population;
+var popcountHTML;
 
 function setup(){
     createCanvas(500, 400);
     population = new Population(POPMAX, GENEMAX, MUTATION);
     population.generatePopulation();
     population.generateGenes();
+    popcountHTML = createP();
 }
 
 function draw(){
     background(200);
+    popcountHTML.html("Generation count: "+POPCOUNT);
     population.run(TIME);
     population.checkFitness();
     population.checkMaxX();
     population.drawBest();
+    if(mouseIsPressed){
+        TIME = TIMEMAX;
+    }
     if(TIME >= TIMEMAX){
         population.generateNewPopulation();
         POPCOUNT++;
         TIME = 0;
     }
     TIME++;
+
 }
